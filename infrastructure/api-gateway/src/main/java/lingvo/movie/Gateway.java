e0f2e4877471f3b5712e498d92cc1c0d5ac2f960
yaroslav <nikolaiko.yaroslav@gmail.com>
2016-05-14 17:17:52 +0300

    zuul api-gateway

---
+++ infrastructure/api-gateway/src/main/java/lingvo/movie/Gateway.java
@@ -0,0 +1,18 @@
+package lingvo.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by yaroslav on 5/14/16.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+}
