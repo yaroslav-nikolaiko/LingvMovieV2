2ba171ce18058f2cddf20b0e9fa27b549ab71d76
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-08-18 01:14:37 +0300

    configure Zuul to forward /** to webapp and /api/<service-name> to service-name

--- infrastructure/api-gateway/src/main/java/lingvo/movie/Gateway.java
+++ infrastructure/api-gateway/src/main/java/lingvo/movie/Gateway.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by yaroslav on 5/14/16.
@@ -14,5 +16,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class Gateway {
     public static void main(String[] args) {
         SpringApplication.run(Gateway.class, args);
+    }
+
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper(
+                "(?<name>^.+)",
+                    "api/${name}");
     }
 }
