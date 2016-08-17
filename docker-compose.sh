95bf7c23685f66eb9eecc84081cbe764204f0023
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2016-08-18 01:13:18 +0300

    add webapp module

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,9 +3,11 @@
 INFRASTRUCTURE=infrastructure/docker/docker-compose.yml
 ACCOUNT=domain/account-service/docker/docker-compose.yml
 DICTIONARY=domain/dictionary-service/docker/docker-compose.yml
+WEBAPP=domain/webapp/docker/docker-compose.yml
 
 docker-compose \
     -f ${INFRASTRUCTURE} \
     -f ${ACCOUNT} \
     -f ${DICTIONARY} \
+    -f ${WEBAPP} \
     up
---
+++ domain/webapp/src/main/java/lingvo/movie/WebappApplication.java
@@ -0,0 +1,30 @@
+package lingvo.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * Created by yaroslav on 07.03.16.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebappApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping(path = "index")
+    public static class HelloWorld{
+
+        @RequestMapping(method = GET)
+        public String helloWorld() {
+            return "Hello Webapp Service";
+        }
+    }
+}
